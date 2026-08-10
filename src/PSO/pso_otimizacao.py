d8bf855c9f36a34bfb8f9bdbd97d704195316741
adonaiviedondon <vieiraadonai@gmail.com>
2026-08-10 00:37:47 -0300

    Update pso_otimizacao.py

--- src/PSO/pso_otimizacao.py
+++ src/PSO/pso_otimizacao.py
@@ -26,9 +26,9 @@ def iniciar(self,funcao,bounds,tamanho_populacao = 50,tamanho_problema = 12):
     self.individual_best_position = np.copy(self.posicoes)
     self.individual_best_fitness = np.copy(self.fitness)
 
-    index_global = int(np.argmin(self.fitness))#pontuação minima para melhor resultado possivel
-    self.global_best_fitness = np.copy(self.fitness[index_global])#posiçao e valor exato da melhor particula
-    self.global_best_position = np.copy(self.posicoes[index_global])#posiçao e valor exato da melhor particula
+    ind_global = int(np.argmin(self.fitness))#pontuação minima para melhor resultado possivel
+    self.global_best_fitness = np.copy(self.fitness[ind_global])#posiçao e valor exato da melhor particula
+    self.global_best_position = np.copy(self.posicoes[ind_global])#posiçao e valor exato da melhor particula
 
 def execucao_ciclo(self):
     #extrainod os limites e numerom de particulas
@@ -37,9 +37,41 @@ def execucao_ciclo(self):
     r1 =np.random.rand(n,self.tamanho_problema)
     r2 =np.random.rand(n,self.tamanho_problema)
 
-    self.velocidades = (self.w  * self.velocidades
-            + self.c1 * r1 * (self.individual_best_position - self.posicoes)
-            + self.c2 * r2 * (self.global_best_position  - self.posicoes))
+    self.velocidades = (self.w  * self.velocidades+ self.c1 * r1 * (self.individual_best_position - self.posicoes) + self.c2 * r2 * (self.global_best_position  - self.posicoes))
+    self.posicoes = np.clip(self.posicoes + self.velocidades,v1,v2)
+    self.fitness = np.array(self.funcao(i) for i in self.posicoes)
+
+    melhora = self.fitness < self.individual_best_fitness
+    self.individual_best_position[melhora] = self.posicoes[melhora]
+    self.individual_best_fitness[melhora] = self.fitness[melhora]
+    ind_melhor = int(np.argmin(self.fitness))
+    if self.fitness[ind_melhor] < self.melhor_global:
+        self.global_best_fitness = float(self.fitness[ind_melhor])
+        self.global_best_position = np.copy(self.posicoes[ind_melhor])
+    self.historico.fitness.append(self.global_best_fitness)
+    return self.global_best_fitness
+
+def obter_melhor_fitness(self):
+    return self.global_best_fitness
+
+def obter_melhor_solucao(self):
+    return self.global_best_position
+
+def obter_estado(self):
+    return{
+        'melhor_fitness' : self.global_best_fitness,
+        'media_fitness' : float(np.mean(self.fitness)),
+        'desvio_padrao' : float(np.std(self.fitness)),
+        'diversidade': float(np.mean(np.std(self.posicoes, axis=0))),
+        'taxa_melhoria' : self.taxa_melhoria(),
+    }
+
+def taxa_melhoria(self):
+    if len(self.historico_fitness) < 12:
+        return 1.0
+    ultimos = self.historico_fitness[-12:]
+    return abs(ultimos[0] - ultimos[-1]) / (abs(ultimos[0]) + 1e-10)
+    
 
 
 
